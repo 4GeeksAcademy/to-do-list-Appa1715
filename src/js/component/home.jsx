412ccd2142af5023aa0817082ed7bb56efe7bb38
Appa1715 <128950756+Appa1715@users.noreply.github.com>
2023-07-13 00:16:35 +0000

    almost there

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [ inputValue, setInputValue ] = useState ("")
+	const [ list, setlist ] = useState ()
+
 	return (
 		<div className="body container">
 			<div className="list">
 				<h1>My TO-DO List!</h1>
 				<ul>
 					<li>
-						<input type="text" placeholder="What we need to do?" />
-					</li>
-					<li>
-						<input type="text" placeholder="What we need to do?" />
-					</li><li>
-						<input type="text" placeholder="What we need to do?" />
-					</li><li>
-						<input type="text" placeholder="What we need to do?" />
+						<input type="text" 
+						onChange={(e)=> setInputValue(e.target.value)}
+						value={inputValue}
+						onKeyDown={(e)=>
+							if (e.key == "Enter") {  
+							 setlist(list.concat(inputValue));
+							 setInputValue("");}}}
+						placeholder="What we need to do?" />
 					</li>
 				</ul>
 			</div>
