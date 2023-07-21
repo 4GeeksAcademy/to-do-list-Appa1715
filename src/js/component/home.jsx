0ecd213ca4bd611b9905e4ac4406b7bf285e9984
Appa1715 <128950756+Appa1715@users.noreply.github.com>
2023-07-21 01:39:58 +0000

    almost done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -17,13 +17,20 @@ const Home = () => {
 						<input type="text" 
 						onChange={(e)=> setInputValue(e.target.value)}
 						value={inputValue}
-						onKeyDown={(e)=>
-							if (e.key == "Enter") {  
-							 setlist(list.concat(inputValue));
-							 setInputValue("");}}}
-						placeholder="What we need to do?" />
+						onKey={(e)=>{
+							if (e.key === "Enter"){
+								setlist(list.concat(inputValue));}
+							}
+						}
+						placeholder="What do we need to do?"/>
 					</li>
+					{list.map((item, index)=> (
+						<li>{item} <i class="fa-solid fa-eraser">onClick={()=> setlist(list.filter((t, currentIndex)=> index))}</i> 
+						</li>
+					))
+					};
 				</ul>
+				<div>{list.length} tasks left. </div>
 			</div>
 		</div>
 	);
