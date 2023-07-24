5c165f67c1f74b0992f1cb069ef19e1d88e84173
Appa1715 <128950756+Appa1715@users.noreply.github.com>
2023-07-24 00:15:32 +0000

    i have finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,35 +1,61 @@
-import React, {useState} from "react";
+import React, {useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
-	const [ inputValue, setInputValue ] = useState ("")
-	const [ list, setlist ] = useState ()
-
+	const [input, setInput] = useState("")
+	const [list, setlist] = useState([])
+	const [numberTasks, setTasks] = useState("Nothing to do, add a ")
+	
+	const handleSubmit = () => {
+		
+		if (list !== "") {
+			setlist([input, ...list])
+			setTasks(list.length+1)
+            setInput("")
+		}
+	}
+	const handleDelete = (id) => {
+		const dellist = list.filter((li, index)=> index !== id);
+		
+		setlist([...dellist])
+		setTasks(list.length-1)
+		
+		list.length-1 === 0 ? setTasks("You are free now, add a "):null
+	}
+	
 	return (
 		<div className="body container">
 			<div className="list">
-				<h1>My TO-DO List!</h1>
-				<ul>
-					<li>
-						<input type="text" 
-						onChange={(e)=> setInputValue(e.target.value)}
-						value={inputValue}
-						onKey={(e)=>{
-							if (e.key === "Enter"){
-								setlist(list.concat(inputValue));}
-							}
+				<h1>My To-Do List!</h1>
+				<div>
+				
+						<input className="input" type="text" placeholder="What we need to do?" 
+						value={input} 
+						onChange={(e)=>{setInput(e.target.value)}} 
+						onKeyDown={(e)=>{
+							if(e.keyCode === 13){
+								handleSubmit();	
 						}
-						placeholder="What do we need to do?"/>
-					</li>
-					
-				</ul>
-				<div> tasks left. </div>
+					}}/>
+			
+					<ul className="uli">
+						{
+							list.map((t, index) => (
+								<li key={index}>
+									<label>{t}</label>
+										<i className="fa-regular fa-trash-can">	
+										<button className="btnClose" onClick={()=>handleDelete(index)}></button></i>
+								</li>
+							))
+						}
+					</ul>
+					<label className="footer">{numberTasks + "task"} </label>
+				</div>
 			</div>
 		</div>
 	);
 };
-
 export default Home;
